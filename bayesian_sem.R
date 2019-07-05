3397fbc138777a3aa1d51393c0e3d0c6907af55c
theogarcia <t.garcia64@hotmail.fr>
2019-07-05 16:49:47 +0200

    More models...

--- bayesian_sem.R
+++ bayesian_sem.R
@@ -37,6 +37,7 @@ attach(data)
 #################################### Bayesian SEM ###############################################
 ##########  Linear model and quantile linear model 
 
+##### Mackerel
 fitmack<-brm(bf(H_0~Mack),data=data)
 
 summary(fitmack)
@@ -47,19 +48,99 @@ fitmack_q<-brm(bf(H_0~Mack, quantile=0.9),data=data, family=asym_laplace)
 
 summary(fitmack_q)
 plot(fitmack_q)
-plot(marginal_effects(fitmack_q), points = TRUE)
+#plot(marginal_effects(fitmack_q), points = TRUE)
+
+
+##### Coastal current
+fitCC<-brm(bf(H_0~Sal_I1),data=data)
+
+summary(fitCC)
+plot(fitCC)
+plot(marginal_effects(fitCC), points = TRUE)
+
+fitCC_q<-brm(bf(H_0~Sal_I1, quantile=0.9),data=data, family=asym_laplace)
+
+summary(fitCC_q)
+plot(fitCC_q)
+#plot(marginal_effects(fitCC_q), points = TRUE)
+
+##### Hatch timing
+fitHT<-brm(bf(H_0~mean_hatch),data=data)
+
+summary(fitHT)
+plot(fitHT)
+plot(marginal_effects(fitHT), points = TRUE)
+
+fitHT_q<-brm(bf(H_0~mean_hatch, quantile=0.9),data=data, family=asym_laplace)
+
+summary(fitHT_q)
+plot(fitHT_q)
+#plot(marginal_effects(fitHT_q), points = TRUE)
+
+
+
+##### Spawning stock
+fitSSB<-brm(bf(H_0~SSB_H),data=data)
+
+summary(fitSSB)
+plot(fitSSB)
+plot(marginal_effects(fitSSB), points = TRUE)
+
+fitSSB_q<-brm(bf(H_0~SSB_H, quantile=0.9),data=data, family=asym_laplace)
+
+summary(fitSSB_q)
+plot(fitSSB_q)
+#plot(marginal_effects(fitHT_q), points = TRUE)
+
+##### Macth Zoopk
+fitCF<-brm(bf(H_0~Cal_fin),data=data)
+
+summary(fitCF)
+plot(fitCF)
+plot(marginal_effects(fitCF), points = TRUE)
+
+fitCF_q<-brm(bf(H_0~Cal_fin, quantile=0.9),data=data, family=asym_laplace)
+
+summary(fitCF_q)
+plot(fitCF_q)
+#plot(marginal_effects(fitCF_q), points = TRUE)
+
+##### Herring 0
+fitH0<-brm(bf(H_0~Cal_fin+Sal_I1+Mack+SSB_H+mean_hatch),data=data)
+
+summary(fitH0)
+plot(fitH0)
+plot(marginal_effects(fitH0), points = TRUE)
+
+fitH0_q<-brm(bf(H_0~Cal_fin+Sal_I1+Mack+SSB_H+mean_hatch, quantile=0.9),data=data, family=asym_laplace, iter = 2000, chains = 4)
+
+summary(fitH0_q)
+plot(fitH0_q)
+#plot(marginal_effects(fitH0_q), points = TRUE)
+
+
 
 ##### For non linear model-> modify prior
 prior1 <- prior(normal(-1,1), nlpar = "a") +
-  prior(normal(1,1), nlpar = "b")+
-  prior(normal(10000,100), nlpar = "c")
+  prior(normal(1,1), nlpar = "b")
 
-fit1 <- brm(bf(H_R2 ~ a * Cc_H^2 + b * Cc_H  + c, a + b +c ~ 1, nl = TRUE),
+fit1 <- brm(bf(H_R2 ~ a * Cc_H^2 + b * Cc_H  , a + b ~ 1, nl = TRUE),
             data = data, prior = prior1)
 
-summary(fit1)
-plot(fit1)
-plot(marginal_effects(fit1), points = TRUE)
+
+
+prior <- prior(normal(10000, 100), class = Intercept)+
+          prior(normal(0, 10), class = b)+
+          prior(cauchy(0, 1), class = sigma)
+
+fit<-brm(bf(H_R2 ~ 1 + Cc_H + I(Cc_H^2)),
+    data = data, prior = prior,family = gaussian,)
+
+
+
+summary(fit)
+plot(fit)
+plot(marginal_effects(fit), points = TRUE)
 
 #########FIT QUADRATIC FUNCTION AND QUANTILE QUADRATIC FUNCTION ##############
 # start with just a simple quadratic approach
